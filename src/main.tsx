3b958c16bbf41c83508e73557feb6969da302242
Naveen G <naveen@basikmarketing.in>
2024-11-22 12:37:50 +0530

    fixed it

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,32 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.tsx';
-import Viewer from './pages/Viewer.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  Route,
+  Router,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import App from "./App.tsx";
+import Viewer from "./pages/Viewer.tsx";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/viewer',
-    element: <Viewer />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+//   {
+//     path: '/viewer',
+//     element: <Viewer />,
+//   },
+// ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
+  // <StrictMode>
+  //   <RouterProvider router={router} />
+  // </StrictMode>
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
